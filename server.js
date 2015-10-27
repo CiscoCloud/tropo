89c0c88a0b6d0c443b6a2e21dd13743a9478b3b4
Vijay <Vjpscjp@gmail.com>
2015-10-27 16:32:35 -0400

    c4

--- server.js
+++ server.js
@@ -5,15 +5,17 @@
 
 var tropowebapi = require('./lib/tropo-webapi');
 var express = require('express');
-var app = express.createServer();
+var bodyParser = require('body-parser')
+var app = express();
+var request = require('request');
 
-/**
- * Required to process the HTTP body
- * req.body has the Object while req.rawBody has the JSON string
- */
-app.configure(function(){
-	app.use(express.bodyDecoder());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+ app.get('/', function (req, res) {
+    res.send('Hello from Cisco Shipped!');
 });
+	 
 
 app.post('/', function(req, res){
 	// Create a new instance of the TropoWebAPI object.
@@ -28,18 +30,43 @@ app.post('/', function(req, res){
 	// Action classes can be passes as parameters to TropoWebAPI class methods.
 	// use the ask method https://www.tropo.com/docs/webapi/ask.htm
 	tropo.ask(choices, 3, false, null, "foo", null, true, say, 5, null);
+	
+	tropo.say("process 1"); 
+	
 	// use the on method https://www.tropo.com/docs/webapi/on.htm
 	tropo.on("continue", null, "/answer", true);
 	
+	tropo.say("process 2"); 
     res.send(tropowebapi.TropoJSON(tropo));
 });
 
-app.post('/answer', function(req, res){
-	// Create a new instance of the TropoWebAPI object.
-	var tropo = new tropowebapi.TropoWebAPI();
-	tropo.say("Your zip code is " + req.body['result']['actions']['interpretation']);
+getWeather=function(zip, callback){
+	request('http://api.openweathermap.org/data/2.5/weather?zip='+zip+'&appid=a8f81765ac74e18e357c9496ac295aad', function (error, response, body) {
+	if (!error && response.statusCode == 200) {
+    callback(body)
+	 
+	}});
 	
-	res.send(tropowebapi.TropoJSON(tropo));
+};
+
+app.post('/answer', function(req, res){	
+	 var tropo = new tropowebapi.TropoWebAPI();
+	//console.log(req.body['result']['actions']['interpretation'])
+	var zip=req.body.result.actions.interpretation;
+	tropo.say("Fetching weather information for your zip code "+ zip);
+	getWeather(zip,function(response){
+		var j= JSON.parse(response)
+			if(j.cod==200){
+				console.log(j.weather[0].description);
+				tropo.say("Weather is ! "+j.weather[0].description);
+			}
+			else{
+				console.log(j.message);
+				tropo.say( "Oops ! "+ j.message);
+			}	
+		 res.send(tropowebapi.TropoJSON(j));
+	});
+	
 });
 
 app.listen(3000);
